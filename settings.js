91c5364be3d6da8ff60c6157be1bb141c58e5bde
svsaini766-droid <s.vsaini766@gmail.com>
2026-07-14 13:13:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,13 +8,13 @@ const settings = {
     "mindserver_port": 8080,
     "auto_open_ui": true, // opens UI in browser on startup
     
-    "base_profile": "llama", // survival, assistant, creative, or god_mode
+    "base_profile": "./profiles/llama.json", // survival, assistant, creative, or god_mode
     "profiles": [
         "./andy.json",
         // "./profiles/gpt.json",
         // "./profiles/claude.json",
         // "./profiles/gemini.json",
-        "./profiles/llama.json",
+        //"./profiles/llama.json",
         // "./profiles/qwen.json",
         // "./profiles/grok.json",
         // "./profiles/mistral.json",
